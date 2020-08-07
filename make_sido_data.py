936bef3ab0b147820491e0400ddd73f419b1a2a0
fwani <seungfwani@gmail.com>
2020-08-07 19:56:24 +0900

    Update dong geometry maker

--- make_sido_data.py
+++ make_sido_data.py
@@ -1,27 +1,81 @@
 # coding: utf-8
 import os
+import glob
+import json
+import shapely
+import pathlib
+import csv
 import pandas as pd
 import geopandas as gpd
 
 from shapely import wkt
 
 home = os.getenv('GEO_DATA_HOME')
-spark_home = os.getenv('SPARK_HOME')
+shape_files = glob.glob(os.path.join(home, 'data/shape/*.shp'))
+print(shape_files)
 export_path = os.path.join(home, 'data/dong')
 
-dorocode_df = pd.read_csv(os.path.join(home, 'data/address/개선_도로명코드_전체분.csv'), sep='|', encoding='utf-8')
-juso_df = pd.read_csv(os.path.join(home, 'data/address/주소_서울특별시.csv'), sep='|', encoding='utf-8')
+pathlib.Path(export_path).mkdir(parents=True, exist_ok=True)
+
+dorocode_df = pd.read_csv(os.path.join(home, 'data/address/개선_도로명코드_전체분.csv'), sep='|', encoding='utf-8', dtype=str)
+
+juso_df = pd.read_csv(os.path.join(home, 'data/address/주소_서울특별시.csv'), sep='|', encoding='utf-8', dtype=str)
+
+jiri_df = gpd.read_file(shape_files[0], sep='|')
+
+joined_df = pd.merge(dorocode_df, juso_df, on=['도로명코드', '읍면동일련번호'], how='left')
+joined_df = pd.merge(jiri_df, joined_df, left_on='BAS_ID', right_on='기초구역번호', how='left')
+joined_df['geometry'].crs = 5179
+print(joined_df.columns)
+
+select_cols = ['시도명', '시도명_로마자', '시군구명',
+       '시군구명_로마자', '읍면동명', '읍면동명_로마자',
+       'geometry']
+
+joined_df = joined_df[select_cols]
+result = []
+for row in joined_df[['시군구명', '읍면동명']].drop_duplicates().iterrows():
+    print(row[1][0], row[1][1])
+    print(joined_df[(joined_df['시군구명'] == row[1][0]) & (joined_df['읍면동명'] == row[1][1])]['시군구명'].count())
+    result.append(json.dumps(shapely.geometry.mapping(
+        joined_df[
+            (joined_df['시군구명'] == row[1][0])
+            & (joined_df['읍면동명'] == row[1][1])]['geometry'].to_crs(4326).unary_union)))
+
+result_df = joined_df[select_cols[:-1]].drop_duplicates()
+result_df['geometry'] = result
+
+result_df.to_csv(
+            os.path.join(export_path, '{}.csv'.format('서울특별시')),
+            sep='|',
+            encoding='utf-8',
+            quoting=csv.QUOTE_NONE,
+            index=False)
+
+
+
+'''
 jiri_df = pd.read_csv(os.path.join(home, 'data/jiri/서울특별시.csv'), sep='|', encoding='utf-8')
 jiri_df['GEOMETRY'] = jiri_df['GEOMETRY'].apply(wkt.loads)
-jiri_df = gpd.GeoDataFrame(jiri_df, crs='epsg:4326')
+jiri_df = gpd.GeoDataFrame(jiri_df, crs={'init': 'epsg:4326'}, geometry='GEOMETRY')
 
 joined_df = pd.merge(dorocode_df, juso_df, on=['도로명코드', '읍면동일련번호'], how='left')
 joined_df = pd.merge(joined_df, jiri_df, left_on='기초구역번호', right_on='BAS_ID', how='left')
 
-print(joined_df.dropna(subset=['GEOMETRY']))
-
-
+# 읍면동
+select_cols = ['시도명', '시도명_로마자', '시군구명', '시군구명_로마자', '읍면동명', '읍면동명_로마자', 'GEOMETRY']
+result_df = joined_df[select_cols].dropna(subset=['GEOMETRY'])
+print(result_df[result_df['읍면동명'] == '문정동']['GEOMETRY'][0])
+print(result_df[result_df['읍면동명'] == '문정동']['GEOMETRY'].unary_union)
 '''
+'''
+result_df.to_csv(
+            os.path.join(export_path, '{}.csv'.format('서울특별시')),
+            sep='|',
+            encoding='utf-8',
+            index=False)
+
+
 sido_list = dorocode_df.select('시도명').distinct().collect()
 for sido in sido_list[:1]:
     juso_df = df_reader.load(os.path.join(home, 'data/address/주소_{}.csv'.format(sido['시도명'])))
@@ -36,8 +90,6 @@ for sido in sido_list[:1]:
                 [joined_df['기초구역번호'] == jiri_df['BAS_ID']]).dropna(subset='시도명')
     print(joined_df.count())
 
-    # 읍면동
-    select_cols = ['시도명', '시도명_로마자', '시군구명', '시군구명_로마자', '읍면동명', '읍면동명_로마자', '도로명', '기초구역번호', 'GEOMETRY']
     joined_df.select(select_cols)\
         .write.mode('overwrite').format("com.databricks.spark.csv")\
         .option("header", "true")\
