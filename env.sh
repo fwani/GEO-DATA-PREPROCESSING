4353d6bc25c3ba079fc6f70e0f4df81e42275270
fwani <seungfwani@gmail.com>
2020-08-12 14:06:42 +0900

    Remove spark environment at env.sh

--- env.sh
+++ env.sh
@@ -2,16 +2,8 @@
 
 export GEO_DATA_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-SPARK_VERSION=2.4.4
-export SPARK_HOME=$GEO_DATA_HOME/thirdparty/spark-${SPARK_VERSION}-bin-hadoop2.7
-
 # Thirdparty library
 export PYTHONPATH=$GEO_DATA_HOME:$PYTHONPATH
-
-# For Spark
-export PYSPARK_PYTHON=python3
-
-export PYTHONPATH=$SPARK_HOME/python:$PYTHONPATH
 
 export OS="`uname`"
 case $OS in
